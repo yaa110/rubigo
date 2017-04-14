3f6d6f2a5faaa1c2efdae89b9c04449808d0d1b1
yaa110 <yaa110@gmail.com>
2017-04-14 22:07:36 +0430

    Print errors in stderr and flush the output in verbose mode

--- src/inner/logger.rs
+++ src/inner/logger.rs
@@ -3,6 +3,7 @@ extern crate time;
 use ansi_term::Color::{Red, Fixed, Yellow};
 use std::process;
 use std::fmt::Display;
+use std::io::{self, Write};
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
 pub enum Verbosity {
@@ -26,12 +27,13 @@ impl Logger {
     pub fn verbose<T: Display>(&self, title: &str, msg: T) {
         if self.verbosity == Verbosity::High {
             println!("[{}] {} {}", Fixed(8).paint(time::strftime("%T", &time::now()).unwrap_or(String::from("00:00:00"))), Yellow.paint(title), msg);
+            let _ = io::stdout().flush();
         }
     }
 
     pub fn error<T: Display>(&self, err: T) {
         if self.verbosity != Verbosity::None {
-            println!("{} {}", Red.paint("error:"), err);
+            let _ = writeln!(&mut io::stderr(), "{} {}", Red.paint("error:"), err);
         }
     }
 
