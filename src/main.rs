4dd61a0405c1c95209a910ccac8109a7c2721149
Navid <yaa110@gmail.com>
2018-12-15 14:03:10 +0330

    Update version to 1.0.4

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ use std::process;
 use controller::*;
 use inner::logger::{Logger, Verbosity};
 
-const VERSION: &'static str = "1.0.3";
+const VERSION: &'static str = "1.0.4";
 
 fn main() {
     let matches = App::new("Rubigo")
